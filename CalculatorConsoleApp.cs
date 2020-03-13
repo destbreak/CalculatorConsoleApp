c807a1a2fa04e8ec8b3f43b17d27efa7eeb09a2f
Reynaldi Hadiansyah <rey.hadiansyah7th@gmail.com>
2020-03-13 10:33:46 +0700

    Add - Input in 'main' Structure

--- CalculatorConsoleApp.cs
+++ CalculatorConsoleApp.cs
@@ -12,8 +12,16 @@ namespace CalculatorConsoleApp
         {
             Console.Title = "Aplikasi Kalkulator";
 
-            int a = 10;
-            int b = 6;
+            // int a = 10;
+            // int b = 6;
+
+            Console.Write("Input Nilai A = ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Input Nilai B = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
 
             Console.WriteLine("Hasil Penambahan " + a + " + " + b + " = " + Penambahan(a, b));
             Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
